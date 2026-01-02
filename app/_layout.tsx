2b58c91c69d62f6cc3671e6aba0b7f34550b47da
Simon Wachira <kimaniwachira03@gmail.com>
2026-01-02 15:15:23 +0300

    update: added tokencache to the basic starter template

---
+++ app/_layout.tsx
@@ -0,0 +1,77 @@
+import {
+  DarkTheme,
+  DefaultTheme,
+  Theme,
+  ThemeProvider,
+} from "@react-navigation/native";
+import { useFonts } from "expo-font";
+import { Slot, Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import {StatusBar} from "expo-status-bar";
+import { useEffect } from "react";
+import "react-native-reanimated";
+import { ClerkProvider,ClerkLoaded } from "@clerk/clerk-expo";
+
+import { useColorScheme } from "react-native";
+import { tokenCache } from "@/cache";
+
+const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY;
+
+// Prevent auto hide splash screen before app is ready
+SplashScreen.preventAutoHideAsync();
+
+
+export default function RootLayout() {
+  const colorScheme = useColorScheme();
+  const [loaded] = useFonts({
+    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
+  });
+
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded]);
+
+  if (!loaded) {
+    return null;
+  }
+
+  if (!publishableKey) {
+    throw new Error("EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY is not set");
+  }
+  const CustomDefaultTheme: Theme = {
+    ...DefaultTheme,
+    dark: false,
+    colors: {
+      primary: "rgb(0, 122, 255)", // System Blue
+      background: "rgb(242, 242, 247)", // Light mode background
+      card: "rgb(255, 255, 255)", // White cards/surfaces
+      text: "rgb(0, 0, 0)", // Black text for light mode
+      border: "rgb(216, 216, 220)", // Light gray for separators/borders
+      notification: "rgb(255, 59, 48)", // System Red
+    },
+  };
+
+  const CustomDarkTheme: Theme = {
+    ...DarkTheme,
+    colors: {
+      primary: "rgb(10, 132, 255)", // System Blue (Dark Mode)
+      background: "rgb(1, 1, 1)", // True black background for OLED displays
+      card: "rgb(28, 28, 30)", // Dark card/surface color
+      text: "rgb(255, 255, 255)", // White text for dark mode
+      border: "rgb(44, 44, 46)", // Dark gray for separators/borders
+      notification: "rgb(255, 69, 58)", // System Red (Dark Mode)
+    },
+  };
+  return (
+    <ClerkProvider publishableKey={publishableKey} tokenCache={tokenCache}>
+      <ClerkLoaded>
+    <ThemeProvider value={colorScheme === "dark" ? CustomDarkTheme : CustomDefaultTheme}>
+      <Slot />
+      <StatusBar style="auto"/>
+    </ThemeProvider>
+    </ClerkLoaded>
+    </ClerkProvider>
+  );
+}
---
+++ cache.ts
@@ -0,0 +1,27 @@
+import * as SecureStore from "expo-secure-store";
+import { Platform } from "react-native";
+import { TokenCache } from "@clerk/clerk-expo";
+
+const createTokenCache = (): TokenCache => {
+  return {
+    getToken: async (key: string) => {
+      try {
+        const item = await SecureStore.getItemAsync(key);
+        if (item) {
+          console.log(`${key} found in cache`);
+        } else {
+          console.log(`${key} not found in cache`);
+        }
+        return item;
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    saveToken: (key: string, token: string) => {
+      return SecureStore.setItemAsync(key, token);
+    },
+  };
+};
+
+export const tokenCache =
+    Platform.OS !== "web" ? createTokenCache() : undefined;
